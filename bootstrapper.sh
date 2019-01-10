ca6594f71e4b5e5391cdf591d981c3e5851edd06
HeretekRaven <46550909+HeretekRaven@users.noreply.github.com>
2019-01-09 22:37:29 -0600

    Update bootstrapper.sh

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -5,36 +5,36 @@
 #Basic installations(light)
 echo "Starting Base Installation";
 sudo xbps-install -Syu || echo "critical failure";
-sudo xbps-install xorg || echo "critical failure";
-sudo xbps-install alsa-utils || echo "critical failure";
-sudo xbps-install xterm || echo "critical failure";
-sudo xbps-install konsole || echo "critical failure";
-sudo xbps-install xorg-init || echo "critical failure"; #Double check name
-sudo xbps-install i3-gaps || echo "critical failure";
-sudo xbps-install i3status || echo "critical failure";
-sudo xbps-install xorg-twm || echo "critical failure"; #Double check name
-sudo xbps-install ranger || echo "critical failure";
-sudo xbps-install connman || echo "critical failure";
-sudo xbps-install cmst || echo "critical failure";
-sudo xbps-install htop || echo "critical failure";
-sudo xbps-install compton || echo "critical failure";
-sudo xbps-install feh || echo "critical failure";
-sudo xbps-install dmenu || echo "critical failure";
-sudo xbps-install nano || echo "critical failure";
+sudo xbps-install -y xorg || echo "critical failure";
+sudo xbps-install -y alsa-utils || echo "critical failure";
+sudo xbps-install -y xterm || echo "critical failure";
+sudo xbps-install -y konsole || echo "critical failure";
+sudo xbps-install -y xorg-init || echo "critical failure"; #Double check name
+sudo xbps-install -y i3-gaps || echo "critical failure";
+sudo xbps-install -y i3status || echo "critical failure";
+sudo xbps-install -y xorg-twm || echo "critical failure"; #Double check name
+sudo xbps-install -y ranger || echo "critical failure";
+sudo xbps-install -y connman || echo "critical failure";
+sudo xbps-install -y cmst || echo "critical failure";
+sudo xbps-install -y htop || echo "critical failure";
+sudo xbps-install -y compton || echo "critical failure";
+sudo xbps-install -y feh || echo "critical failure";
+sudo xbps-install -y dmenu || echo "critical failure";
+sudo xbps-install -y nano || echo "critical failure";
 
 #The rest of the fun stuff
 echo "Starting Main Installation";
-sudo xbps-install sxiv || echo "critical failure(II)";
-sudo xbps-install chromium || echo "critical failure(II)";
-sudo xbps-install polybar || echo "critical failure(II)";
-sudo xbps-install neofetch || echo "critical failure(II)";
-sudo xbps-install gimp || echo "critical failure(II)";
-sudo xbps-install ImageMagick || echo "critical failure(II)";
-sudo xbps-install  || echo "critical failure(II)";
+sudo xbps-install -y sxiv || echo "critical failure(II)";
+sudo xbps-install -y chromium || echo "critical failure(II)";
+sudo xbps-install -y polybar || echo "critical failure(II)";
+sudo xbps-install -y neofetch || echo "critical failure(II)";
+sudo xbps-install -y gimp || echo "critical failure(II)";
+sudo xbps-install -y ImageMagick || echo "critical failure(II)";
+#sudo xbps-install -y  || echo "critical failure(II)";
 
 #Dot file setup/creation
 echo "Setting up dot files";
-sudo xbps-install git || echo "critical failure(III)";
+sudo xbps-install -y git || echo "critical failure(III)";
 touch .xinitrc; "exec compton &" >> .xinitrc; "exec i3" >> .xinitrc;
 sudo Xorg -configure;
 
